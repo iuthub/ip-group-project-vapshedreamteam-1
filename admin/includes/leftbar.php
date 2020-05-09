87b44979bdfd616adcb09dad8caa6a2d6bf4926d
khaitboevo <khaitboev99@mail.ru>
2020-05-10 04:28:44 +0500

    updated php file

---
+++ admin/includes/leftbar.php
@@ -0,0 +1,31 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu nav_ul">
+			
+				<li class="ts-label">Main</li>
+				<!-- <li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li> -->
+			
+<li><a href="#"><i class=""></i> Brands</a>
+<ul>
+<li><a href="create-brand.php">Create Brand</a></li>
+<li><a href="manage-brands.php">Manage Brands</a></li>
+</ul>
+</li>
+
+<li><a href="#"><i class=""></i> Vehicles</a>
+					<ul>
+						<li><a href="post-avehical.php">Post a Vehicle</a></li>
+						<li><a href="manage-vehicles.php">Manage Vehicles</a></li>
+					</ul>
+				</li>
+				<li><a href="manage-bookings.php"><i class=""></i> Booking Management</a></li>
+
+				<!-- <li><a href="testimonials.php"><i class=""></i>Testimonials</a></li> -->
+				<li><a href="manage-conactusquery.php"><i class="""></i>Conactus Query</a></li>
+				<li><a href="reg-users.php"><i class=""></i> Registered Users</a></li>
+			<!-- <li><a href="manage-pages.php"><i class=""></i>Page Management</a></li> -->
+			<li><a href="update-contactinfo.php"><i class=""></i> Update Contact Info</a></li>
+
+			<li><a href="manage-subscribers.php"><i class=""></i>Subscribers</a></li>
+
+			</ul>
+		</nav>
