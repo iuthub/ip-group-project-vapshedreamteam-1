296b28378af1ed81e62b67b0a5a9d2b46ee80a58
norik99 <norik.shax@gmail.com>
2020-05-09 05:43:14 +0500

    updated

--- includes/footer.php
+++ includes/footer.php
@@ -36,14 +36,11 @@ echo "<script>alert('Something went wrong. Please try again');</script>";
       <div class="row">
       
         <div class="col-md-6">
-          <h6>About Us</h6>
+          <!-- <h6>About Us</h6> -->
           <ul>
 
         
           <li><a href="page.php?type=aboutus">About Us</a></li>
-            <li><a href="page.php?type=faqs">FAQs</a></li>
-            <li><a href="page.php?type=privacy">Privacy</a></li>
-          <li><a href="page.php?type=terms">Terms of use</a></li>
                <li><a href="admin/">Admin Login</a></li>
           </ul>
         </div>
@@ -59,27 +56,6 @@ echo "<script>alert('Something went wrong. Please try again');</script>";
             </form>
             <p class="subscribed-text">*We send great deals and latest auto news to our subscribed users very week.</p>
           </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="footer-bottom">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6 col-md-push-6 text-right">
-          <div class="footer_widget">
-            <p>Connect with Us:</p>
-            <ul>
-              <li><a href="#"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
-              <li><a href="#"><i class="fa fa-twitter-square" aria-hidden="true"></i></a></li>
-              <li><a href="#"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
-              <li><a href="#"><i class="fa fa-google-plus-square" aria-hidden="true"></i></a></li>
-              <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-            </ul>
-          </div>
-        </div>
-        <div class="col-md-6 col-md-pull-6">
-        
         </div>
       </div>
     </div>
